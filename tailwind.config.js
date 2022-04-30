520b8a29f012e9e9581e89c90c274d289134b7f9
JoJo <joevega2020@gmail.com>
2022-04-30 10:54:59 -0500

    History-Login and Register

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,35 @@
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'color-blue': '#0065ff',
+				'h-color-blue': '#00679f',
+			},
+			fontFamily: {
+				BogleWeb1: ['BogleWeb', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+			},
+			width: {
+				86: '21.5rem',
+				90: '22.5rem',
+			},
+		},
+		screens: {
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '768px',
+			// => @media (min-width: 768px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
 	},
 	plugins: [],
 };
